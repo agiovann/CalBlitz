81f1cb5ae9df253cd6c2680dc2a991f45466bd8f
Andrea Giovannucci <agiovann@AndreaGiovannucci2015RetinaMacbookPro.local>
2016-03-03 11:53:08 -0500

    merge

--- DemoMotionCorrection.py
+++ DemoMotionCorrection.py
@@ -19,42 +19,30 @@ try:
 except:
     print "Probably not a Ipython interactive environment" 
 
-
 #%% define movie
-
-
-filename='demoMovie.tif'
+filename='movies/demoMovie_PC.tif'
 frameRate=15.62;
 start_time=0;
-
 #%%
 filename_py=filename[:-4]+'.npz'
 filename_hdf5=filename[:-4]+'.hdf5'
 filename_mc=filename[:-4]+'_mc.npz'
-
 #%% load movie
 m=cb.load(filename, fr=frameRate,start_time=start_time);
-#%% load submovie
-#m=cb.load(filename, fr=frameRate,start_time=start_time,subindices=range(0,1500,10));
-#%% save movie python format. Less efficient than hdf5 below
-#m.save(filename_py)
-#m=cb.load(filename_py); 
+#%% load portion of the movie or only some channels
+#m=cb.load(filename, fr=frameRate,start_time=start_time,subindices=range(0,1500,10)); 
 #%% save movie hdf5 format. fastest
 m.save(filename_hdf5)
-#%%
-m.play(fr=100,gain=5.0,magnification=1,backend='pylab')
-
-#%% concatenate movies (it will add to the original movie)
-#conc_mov=cb.concatenate([m,m])
-    
+#%% backend='opencv' is much faster
+m.play(fr=100,gain=5.0,magnification=1,backend='pylab')    
 #%% automatic parameters motion correction
-max_shift_h=10;
-max_shift_w=10;
+max_shift_h=10; # maximum allowed shifts in y
+max_shift_w=10;  # maximum allowed shifts in x
 m=cb.load(filename_hdf5); 
 m,shifts,xcorrs,template=m.motion_correct(max_shift_w=max_shift_w,max_shift_h=max_shift_h, num_frames_template=None, template = None,method='opencv')
-#m,shifts,xcorrs,template=m.motion_correct(max_shift_w=max_shift_w,max_shift_h=max_shift_h, num_frames_template=None, template = None,method='skimage')
 
 
+# removie the borders 
 max_h,max_w= np.max(shifts,axis=0)
 min_h,min_w= np.min(shifts,axis=0)
 m=m.crop(crop_top=max_h,crop_bottom=-min_h+1,crop_left=max_w,crop_right=-min_w,crop_begin=0,crop_end=0)
@@ -70,7 +58,7 @@ pl.ylabel('cross correlation')
 pl.xlabel('Frames')
 
 #%%
-#m.play(fr=50,gain=5.0,magnification=1)   
+m.play(fr=50,gain=5.0,magnification=1)   
 
 #%% IF YOU WANT MORE CONTROL USE THE FOLLOWING
 # motion correct for template purpose. Just use a subset to compute the template
@@ -90,12 +78,9 @@ template=(np.nanmedian(submov,axis=0))
 shifts,xcorrs=m.extract_shifts(max_shift_w=max_shift_w, max_shift_h=max_shift_h, template=template, method='opencv')  #
 m=m.apply_shifts(shifts,interpolation='cubic')
 template=(np.median(m,axis=0))
-#pl.subplot(1,2,1)
-#pl.imshow(templ,cmap=pl.cm.gray)
-#pl.subplot(1,2,2)
+
 pl.imshow(template,cmap=pl.cm.gray)
 #%% now use the good template to correct
-
 max_shift_h=10;
 max_shift_w=10;
 
@@ -124,15 +109,11 @@ meta_data=m.meta_data[0];
 shifts,min_val_add,xcorrs=[meta_data[x] for x in ['shifts','min_val_add','xcorrs']]
 m=m.apply_shifts(shifts,interpolation='cubic')
 
-#%% crop borders created by motion correction
-    
+#% crop borders created by motion correction 
 max_h,max_w= np.max(shifts,axis=0)
 min_h,min_w= np.min(shifts,axis=0)
 m=m.crop(crop_top=max_h,crop_bottom=-min_h+1,crop_left=max_w,crop_right=-min_w,crop_begin=0,crop_end=0)
 
-
 #%% play movie
-#m.play(fr=50,gain=3.0,magnification=1)
-#%% visualize average
-#meanMov=(np.mean(m,axis=0))
-#pl.imshow(meanMov,cmap=pl.cm.gray,vmax=200)
+m.play(fr=50,gain=3.0,magnification=1)
+
