1c89c4579d99ea0a6e641f5e7c26938e6a73c78a
Andrea Giovannnucci <andrea.giovannucci@gmail.com>
2016-05-20 20:38:47 -0400

    added parsing of tif file image information

--- calblitz/utils.py
+++ calblitz/utils.py
@@ -18,6 +18,7 @@ import calblitz as cb
 import numpy as np
 from ipyparallel import Client
 import os
+import tifffile
 #%%
 def playMatrix(mov,gain=1.0,frate=.033):
     for frame in mov: 
@@ -226,3 +227,46 @@ def process_movie_parallel(arg_in):
         #sys.stdout = sys.__stdout__ 
         
     return fname[:-3]        
+#%% 
+def val_parse(v):
+    # parse values from si tags into python objects if possible
+    try:
+        return eval(v)
+    except:
+        if v == 'true':
+            return True
+        elif v == 'false':
+            return False
+        elif v == 'NaN':
+            return np.nan
+        elif v == 'inf' or v == 'Inf':
+            return np.inf
+
+        else:
+            return v
+
+def si_parse(imd):
+
+    # parse image_description field embedded by scanimage
+    imd = imd.split('\n')
+    imd = [i for i in imd if '=' in i]
+    imd = [i.split('=') for i in imd]
+    imd = [[ii.strip(' \r') for ii in i] for i in imd]
+    imd = {i[0]:val_parse(i[1]) for i in imd}
+    return imd
+
+#%%
+def get_image_description_SI(fname):
+    """Given a tif file acquired with Scanimage it returns a dictionary containing the information in the image description field
+    """
+    image_descriptions=[]
+    tf=tifffile.TiffFile(fname)
+    for idx,pag in enumerate(tf.pages):
+            if idx%1000==0:
+                print(idx)
+    #        i2cd=si_parse(pag.tags['image_description'].value)['I2CData']
+            field=pag.tags['image_description'].value
+            
+            image_descriptions.append(si_parse(field))
+    
+    return image_descriptions
